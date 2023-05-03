d6878102413da52baf096b60c83ed560d09c85b7
Shivangi <shivangit1111@gmail.com>
2023-05-03 15:52:41 +0530

    style: create top menu bar

--- app/src/main/java/com/example/musicplayerclone/ToDoActivity.kt
+++ app/src/main/java/com/example/musicplayerclone/ToDoActivity.kt
@@ -1,12 +1,17 @@
 package com.example.musicplayerclone
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toast
 import com.example.musicplayerclone.databinding.ActivityToDoBinding
 
 class ToDoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityToDoBinding
+
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,5 +19,64 @@ class ToDoActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+
+        binding.dots.setOnClickListener {
+            val popupMenu = PopupMenu(this, binding.dots)
+            popupMenu.inflate(R.menu.app_bar_menu)
+            popupMenu.setOnMenuItemClickListener {
+                // Do something when a menu item is clicked
+                when (it.itemId) {
+
+
+                    R.id.taskList -> Toast.makeText(
+                        this,
+                        "You clicked on taskList",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+
+                    R.id.addINBatch -> Toast.makeText(
+                        this,
+                        "You clicked on addINBatch",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+
+                    R.id.removeAdds -> Toast.makeText(
+                        this,
+                        "You clicked on removeAdds",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+
+
+                    R.id.moreApps -> Toast.makeText(
+                        this,
+                        "You clicked on moreApps",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    R.id.inVite -> Toast.makeText(
+                        this,
+                        "You clicked on inVite",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    R.id.settings -> Toast.makeText(
+                        this,
+                        "You clicked on settings",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    R.id.closeApp -> finish()
+
+                }
+                true
+            }
+            popupMenu.show()
+        }
+
     }
+
+
 }
