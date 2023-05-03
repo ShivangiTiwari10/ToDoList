1b9efd20525d9cb0b0b3dcc086c7c1232f90697e
Shivangi <shivangit1111@gmail.com>
2023-05-03 11:04:02 +0530

    chore: make new activity

--- app/src/main/java/com/example/musicplayerclone/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerclone/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.musicplayerclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.musicplayerclone.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        activityScope.launch {
+            delay(2000)
+
+            val intent = Intent(this@MainActivity, ToDoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 }
