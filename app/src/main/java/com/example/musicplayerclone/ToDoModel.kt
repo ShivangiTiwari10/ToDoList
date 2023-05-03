70319b50535220a3d2a8757e969dd2441df3830c
Shivangi <shivangit1111@gmail.com>
2023-05-03 20:19:16 +0530

    feat: put Entity

--- app/src/main/java/com/example/musicplayerclone/ToDoModel.kt
+++ app/src/main/java/com/example/musicplayerclone/ToDoModel.kt
@@ -1,15 +1,17 @@
 package com.example.musicplayerclone
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class ToDoModel(
 
-    var title:String,
-    var description:String,
+    var title: String,
+    var description: String,
     var category: String,
-    var date:Long,
-    var time:Long,
-    var isFinished : Int = 0,
+    var date: Long,
+    var time: Long,
+    var isFinished: Int = 0,
     @PrimaryKey(autoGenerate = true)
-    var id:Long = 0
+    var id: Long = 0
 )
