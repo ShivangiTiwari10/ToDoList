c7de550bcb15ff041b65463984265efa9b46f6a0
Shivangi <shivangit1111@gmail.com>
2023-05-03 11:15:19 +0530

    feat: implement binding

--- app/src/main/java/com/example/musicplayerclone/ToDoActivity.kt
+++ app/src/main/java/com/example/musicplayerclone/ToDoActivity.kt
@@ -2,10 +2,15 @@ package com.example.musicplayerclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicplayerclone.databinding.ActivityToDoBinding
 
 class ToDoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityToDoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_to_do)
+
+        binding = ActivityToDoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
