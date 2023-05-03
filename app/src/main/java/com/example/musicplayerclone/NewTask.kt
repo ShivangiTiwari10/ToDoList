250194810b055cdbb27c6850a6db090298d5a4aa
Shivangi <shivangit1111@gmail.com>
2023-05-03 20:20:05 +0530

    feat: put onClick method

--- app/src/main/java/com/example/musicplayerclone/NewTask.kt
+++ app/src/main/java/com/example/musicplayerclone/NewTask.kt
@@ -2,9 +2,10 @@ package com.example.musicplayerclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.musicplayerclone.databinding.ActivityNewTaskBinding
 
-class NewTask : AppCompatActivity() {
+class NewTask : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityNewTaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +13,34 @@ class NewTask : AppCompatActivity() {
         binding = ActivityNewTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View?) {
+
+        if (v != null) {
+            when (v.id) {
+                R.id.dateEdt -> {
+                    setListener()
+                }
+                R.id.timeEdt -> {
+                    setTimeListener()
+                }
+                R.id.saveBtn -> {
+                    saveTodo()
+                }
+            }
+        }
+    }
+
+    private fun saveTodo() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setTimeListener() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setListener() {
+        TODO("Not yet implemented")
     }
 }
