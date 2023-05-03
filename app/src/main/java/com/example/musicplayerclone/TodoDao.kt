abb4ad5bef256bfbce54ef2eb97a0be1cf3fe62f
Shivangi <shivangit1111@gmail.com>
2023-05-03 20:20:35 +0530

    feat: make dao interface

---
+++ app/src/main/java/com/example/musicplayerclone/TodoDao.kt
@@ -0,0 +1,20 @@
+package com.example.musicplayerclone
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+
+interface TodoDao {
+
+    @Insert()
+    suspend fun insertTask(todoModel: ToDoModel):Long
+
+    @Query("Select * from ToDoModel where isFinished == 0")
+    fun getTask(): LiveData<List<ToDoModel>>
+
+    @Query("Update ToDoModel Set isFinished = 1 where id=:uid")
+    fun finishTask(uid:Long)
+
+    @Query("Delete from TodoModel where id=:uid")
+    fun deleteTask(uid:Long)
+}
